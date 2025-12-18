975f8125fff60b2f4aa19e07b2a6d8d34ac440b3
eyalbh8 <eyalbh8@gmail.com>
2025-12-18 18:00:00 +0200

    Refactor LocalTunnelService to improve local tunnel management and streamline development workflow. Update related configurations and enhance data handling capabilities.

---
+++ src/utils/request.id.middleware.ts
@@ -0,0 +1,17 @@
+// src/middleware/request-id.middleware.ts
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { ClsService } from 'nestjs-cls';
+
+@Injectable()
+export class RequestIdMiddleware implements NestMiddleware {
+  constructor(private readonly cls: ClsService) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const requestId = uuidv4();
+
+    this.cls.set('requestId', requestId);
+    this.cls.set('version', 'v1');
+    next();
+  }
+}
